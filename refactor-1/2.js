8bfa5dee7d01af142c1117b3ba5ef3bbf2d3cef9
nxxc <nomad.nxxc@gmail.com>
2021-03-22 17:29:04 +0900

    [refactoring-step2] totalAmount refactor

--- refactor-1/2.js
+++ refactor-1/2.js
@@ -2,16 +2,14 @@ const plays = require('./data/plays.json');
 const invoices = require('./data/invoices.json');
 
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명 :${invoice.customer})\n`;
   for (let perf of invoice.performances) {
     result += `${playFor(perf).name} : ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액 : ${usd(totalAmount)}\n`;
+  result += `총액 : ${usd(totalAmount())}\n`;
   result += `포인트 : ${totalVolumeCredits()}점`;
   return result;
 
@@ -60,11 +58,19 @@ function statement(invoice, plays) {
   }
 
   function totalVolumeCredits() {
-    let volumeCredits = 0; //포인트
+    let result = 0; //포인트
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
+  }
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
   }
 }
 
